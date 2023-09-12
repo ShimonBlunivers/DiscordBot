9d911a2596bc3755a76db9e86d153d540801a601
ShimonBlunivers <93725825+ShimonBlunivers@users.noreply.github.com>
2023-09-12 18:18:49 +0200

    Update Token.java

--- nelinkabot/src/main/java/blunivers/bot/Token.java
+++ nelinkabot/src/main/java/blunivers/bot/Token.java
@@ -1,5 +1,5 @@
 package blunivers.bot;
 
 public class Token {
-    static String token = "OTQwMjIxNTM1OTQyMjQyMzI1.GEJ9yL.azZIat6gvRGhrOWhOPB-yNtW73v55xZfE46UIQ";
+    static String token = "token";
 }
